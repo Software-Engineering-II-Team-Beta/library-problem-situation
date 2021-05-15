b163216fca394d39d0533d23f2fc17a613813f62
Karen Santos <karen.asnt@gmail.com>
2021-05-15 18:07:24 -0300

    start projetec front-end in react.js

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
+import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
