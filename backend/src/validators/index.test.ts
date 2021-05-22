1b2ecf03e4786aae227f81b434b0e7acef554842
Igor Tironi <tironiigor@gmail.com>
2021-05-22 08:50:00 -0300

    changed test case description

--- backend/src/validators/index.test.ts
+++ backend/src/validators/index.test.ts
@@ -2,12 +2,12 @@
 import { isPasswordValid } from ".";
 
 // MARK: Implementation
-test("Check if small password is invalid", async (done) => {
+test("Check if small password is rejected", async (done) => {
 	expect(isPasswordValid("123")).toEqual(false);
 	done();
 });
 
-test("Check if 6 digits password is valid", async (done) => {
+test("Check if 6 digits password is accepted", async (done) => {
 	expect(isPasswordValid("123456")).toEqual(true);
 	done();
 });
