ef3aa168978623bdb92999ac33d5736d2c740f2d
Igor Tironi <tironiigor@gmail.com>
2021-05-24 20:16:28 -0300

    added default values for users factory to avoid tests failing for validatio errors

--- backend/src/routes/users/factory.ts
+++ backend/src/routes/users/factory.ts
@@ -3,11 +3,12 @@ import { ICreateUserRequestBody } from ".";
 import faker from "../../_test/faker";
 
 export function generateNewUser(): ICreateUserRequestBody {
+	// TODO: Change default values for faker generated ones. This might result in changes needed to the validation methods
 	return {
 		address: "9080 Isabella Rodovia",
 		cpf: cpf.generate(),
 		email: faker.internet.email(),
-		password: faker.internet.password(),
-		phoneNumber: faker.phone.phoneNumber(),
+		password: "123456",
+		phoneNumber: "(63) 0726-8755",
 	};
 }
