5fd2c4479d5370758178a5bce86c20f507e45d20
Pablo Henrique <pablohenrique.r.c@gmail.com>
2021-05-24 17:45:02 -0300

    Implementa algumas mudanças na tela Home
    - Acrescenta imagem e nome do sistema;
    - Renomeia variáveis para o padrão camelCase;
    - Cria variáveis específicas para distinguir os campos de e-mail e senha de login dos campos de registro;
    - Implementa parcialmente chamadas à API.

--- frontend/src/screens/Home/index.jsx
+++ frontend/src/screens/Home/index.jsx
@@ -1,74 +1,116 @@
 import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 
+import image from '../../assets/image1.png'
+
+import api from '../../services/api'
+
 function Home() {
-  const [email, set_email] = useState("");
-  const [cpf, set_cpf] = useState("");
-  const [address, set_address] = useState("");
-  const [phone, set_phone] = useState("");
-  const [username, set_username] = useState("");
-  const [password, set_password] = useState("");
+  const [signUpEmail, setsignUpEmail] = useState("");
+  const [cpf, setCPF] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
 
   const history = useHistory("");
 
-  function validate_registration_info(info) {
-    // alguma coisa com validação de dados
-    return true;
+  function validateSignUpData() { 
+    if(!!signUpEmail.trim() && 
+      !!cpf.trim() &&
+      !!address.trim() &&
+      !!phone.trim() &&
+      !!username.trim() &&
+      !!signUpPassword.trim()
+    ) return;
+
+    signUp()
   }
 
-  function validate_login_info(info) {
-    // alguma coisa com validação de dados
-    return true;
+  function validateSignInData() {
+    if(!!signInEmail.trim() && !!setSignInPassword.trim()) return;
+
+    signIn()
   }
 
-  function register() {
-    const info = {
-      email: email,
-      cpf: cpf,
-      address: address,
-      phone: phone,
-      username: username,
-      password: password,
-    };
-    if (validate_registration_info(info)) {
-      // alguma coisa no backend
+  async function signUp(formData) {
+    try {
+      const { data } = await api.post("auth/register", {
+        email: formData.signUpEmail,
+        cpf: formData.cpf,
+        address: formData.address,
+        phone: formData.phone,
+        username: formData.username,
+        password: formData.signUpPassword,
+      })
+
+      console.log(data)
+
       history.push("/dashboard");
+    } catch(error) {
+      console.log(error)
     }
   }
 
-  function login() {
-    const info = {
-      username: username,
-      password: password,
-    };
-    if (validate_login_info(info)) {
-      // alguma coisa no backend
+  async function signIn() {
+    try {
+      const { data } = await api.post("auth/login", {
+        email: signInEmail,
+        password: signInPassword
+      })
+
+      // const {
+      //   user,
+      //   token
+      // } = data
+
+      console.log(data)
+
       history.push("/dashboard");
+    } catch(error) {
+      console.log(error)
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        alert(error.response.data.error)
+      }
     }
   }
 
   return (
-    < div className="homepage" >
-      {/* <h1>Sistema de empréstimo - MATA63</h1> */}
-      <input placeholder="Busque um livro" className="searchbar" />
-      <div className="registration">
-        <h2>Cadastre-se</h2>
-        <input placeholder="Email" value={email} onChange={(e) => set_email(e.currentTarget.value)} />
-        <input placeholder="CPF" value={cpf} onChange={(e) => set_cpf(e.currentTarget.value)} />
-        <input placeholder="Endereço" value={address} onChange={(e) => set_address(e.currentTarget.value)} />
-        <input placeholder="Telefone" value={phone} onChange={(e) => set_phone(e.currentTarget.value)} />
-        <div>
-          <input placeholder="Usuário" value={username} onChange={(e) => set_username(e.currentTarget.value)} />
-          <input placeholder="Senha" value={password} onChange={(e) => set_password(e.currentTarget.value)} />
-        </div>
-        <button className="registration" onClick={register}>cadastrar-se</button>
-
-        <h2>Login</h2>
-        <input placeholder="Usuário" value={username} onChange={(e) => set_username(e.currentTarget.value)} />
-        <input placeholder="Senha" value={password} onChange={(e) => set_password(e.currentTarget.value)} />
-        <button className="registration" onClick={login}>login</button>
+    <div className="homepage">
+      <div className="left">
+        <h1 className="title">Sistema de empréstimo - MATA63</h1>
+        <img 
+          className="image"
+          src={image} 
+          alt="" 
+        />
+        <input 
+          className="searchbar"
+          placeholder="Busque um livro" 
+        />
       </div>
-    </div >
+      <div className="registration">
+        <h2 className="subtitle">Cadastre-se</h2>
+        <input placeholder="E-mail" value={signUpEmail} onChange={(e) => setsignUpEmail(e.currentTarget.value)} />
+        <input placeholder="CPF" value={cpf} onChange={(e) => setCPF(e.currentTarget.value)} />
+        <input placeholder="Endereço" value={address} onChange={(e) => setAddress(e.currentTarget.value)} />
+        <input placeholder="Telefone" value={phone} onChange={(e) => setPhone(e.currentTarget.value)} />
+        <div>
+          <input placeholder="Usuário" value={username} onChange={(e) => setUsername(e.currentTarget.value)} />
+          <input placeholder="Senha" value={signUpPassword} onChange={(e) => setSignUpPassword(e.currentTarget.value)} />
+        </div>
+        <button className="registration" onClick={validateSignUpData}>cadastrar-se</button>
+        <h2 className="subtitle">Login</h2>
+        <input placeholder="Usuário" value={signInEmail} onChange={(e) => setSignInEmail(e.currentTarget.value)} />
+        <input placeholder="Senha" value={signInPassword} onChange={(e) => setSignInPassword(e.currentTarget.value)} />
+        <button className="registration" onClick={validateSignInData}>login</button>
+      </div>
+    </div>
   );
 }
 
