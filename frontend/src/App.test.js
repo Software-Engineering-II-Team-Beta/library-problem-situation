dae35bd38edf15b9056031d336292e2630d58c71
Igor Tironi <tironiigor@gmail.com>
2021-06-01 11:05:05 -0300

    added verb to test description

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('if App renders sistem title correctly', () => {
+test('Check if App renders sistem title correctly', () => {
   render(<App />);
 
   const linkElement = screen.getByText(/Sistema de empréstimo - MATA 63/i);
